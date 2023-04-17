9ed4fb1bdc4fd76d4dbc4ff247778b19baa74d34
kesudo <129604329+kesudo@users.noreply.github.com>
2023-04-17 22:51:26 +0800

    Add files via upload

---
+++ 120p/admin/admin_accounts.php
@@ -0,0 +1,63 @@
+<?php
+
+include '../components/connect.php';
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+
+if(!isset($admin_id)){
+   header('location:admin_login.php');
+}
+
+if(isset($_GET['delete'])){
+   $delete_id = $_GET['delete'];
+   $delete_admins = $conn->prepare("DELETE FROM `admins` WHERE id = ?");
+   $delete_admins->execute([$delete_id]);
+   header('location:admin_accounts.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>admin accounts</title>
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <link rel="stylesheet" href="../css/admin_style.css">
+
+</head>
+<body>
+
+<?php include '../components/admin_header.php'; ?>
+
+<section class="accounts">
+
+   <h1 class="heading">admin accounts</h1>
+
+   <div class="box-container">
+
+
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="../js/admin_script.js"></script>
+   
+</body>
+</html>
---
+++ 120p/admin/placed_orders.php
@@ -0,0 +1,68 @@
+<?php
+
+include '../components/connect.php';
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+
+if(!isset($admin_id)){
+   header('location:admin_login.php');
+}
+
+if(isset($_POST['update_payment'])){
+   $order_id = $_POST['order_id'];
+   $payment_status = $_POST['payment_status'];
+   $payment_status = filter_var($payment_status, FILTER_SANITIZE_STRING);
+   $update_payment = $conn->prepare("UPDATE `orders` SET payment_status = ? WHERE id = ?");
+   $update_payment->execute([$payment_status, $order_id]);
+   $message[] = 'payment status updated!';
+}
+
+if(isset($_GET['delete'])){
+   $delete_id = $_GET['delete'];
+   $delete_order = $conn->prepare("DELETE FROM `orders` WHERE id = ?");
+   $delete_order->execute([$delete_id]);
+   header('location:placed_orders.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>placed orders</title>
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <link rel="stylesheet" href="../css/admin_style.css">
+
+</head>
+<body>
+
+<?php include '../components/admin_header.php'; ?>
+
+<section class="orders">
+
+<h1 class="heading">placed orders</h1>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="../js/admin_script.js"></script>
+   
+</body>
+</html>
