7f2909f0d733a7da8a67a3880b4ad79dd1661973
Vinzaghi <ongcamp17@gmail.com>
2023-04-24 23:18:42 +0800

    added folder

--- components/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$db_name = 'mysql:host=localhost;dbname=id20646650_shop_db';
-$user_name = 'id20646650_120pecom';
-$user_password = 'er9~7|+wb3bi4Cr%';
-
-$conn = new PDO($db_name, $user_name, $user_password);
-
-?>
